a16617b4d104e551971fd086bde9f758a6eb1b7e
Jon M <jmat8116@gmail.com>
2017-02-10 11:30:54 -0600

    First commit

---
+++ CSE_214_HW_2/src/EmptyListException.java
@@ -0,0 +1,11 @@
+/**The empty list exception is thrown when ever an operation is done 
+ * on the list but the list does not contain any nodes
+ * @author John
+ * @version 1, July 20, 2016
+ */
+public class EmptyListException extends Exception{
+	
+	public EmptyListException(){
+		super("The list is empty");
+	}
+}
---
+++ CSE_214_HW_5/src/pkg1/FolderExistsException.java
@@ -0,0 +1,12 @@
+package pkg1;
+/**This is an exception class for whether a folder exists
+ * @author John
+ * @version 1, August 14, 2016
+ */
+public class FolderExistsException extends Exception{
+	/**This is the constructor for the FolderExistsException class
+	 */
+	public FolderExistsException(){
+		super();
+	}
+}
